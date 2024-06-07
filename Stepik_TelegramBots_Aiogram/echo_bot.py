ea1cd9ec69c21a3fc33355d4487df6d4454c3d63
Maksim Terentev <maxim.i.terentev@gmail.com>
2024-06-07 09:45:08 +0300

    simplified echo_bot.py: added send_echo function

--- Stepik_TelegramBots_Aiogram/echo_bot.py
+++ Stepik_TelegramBots_Aiogram/echo_bot.py
@@ -10,36 +10,29 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 # Этот хэндлер будет срабатывать на команду "/start"
-# @dp.message(Command(commands=["start"]))
+@dp.message(Command(commands='start'))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
-
 # Этот хэндлер будет срабатывать на команду "/help"
-# @dp.message(Command(commands=['help']))
+@dp.message(Command(commands='help'))
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
         'я пришлю тебе твое сообщение'
     )
 
-
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# Этот хэндлер будет срабатывать на любые сообщения,
 # кроме команд "/start" и "/help"
-# @dp.message()
+@dp.message()
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
-
-
-# Регистрируем хэндлеры
-dp.message.register(process_start_command, Command(commands='start'))
-dp.message.register(process_help_command, Command(commands='help'))
-dp.message.register(send_echo)
-# То есть мы можем обращаться к полю диспетчера, отвечающего за тип сообщения,
-# которое мы хотим обработать хэндлером и вызвать у него метод register,
-# а в качестве аргументов передать ему имя хэндлера и фильтры. Регистрировать
-# хэндлеры также важно в правильном порядке, чтобы обработчики случайно
-# не перехватывали сообщения, которые для них не предназначены.
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(
+            text='Данный тип апдейтов не поддерживается '
+                 'методом send_copy'
+        )
 
 if __name__ == '__main__':
     dp.run_polling(bot)
